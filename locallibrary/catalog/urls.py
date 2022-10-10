570472838557cd96a23586677a3367c2c2db75f9
zazaz <zazaza993@mail.ru>
2022-10-10 21:11:22 +0700

    gg

---
+++ locallibrary/catalog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# странно
+from . import views
+from django.conf.urls import url
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+]
